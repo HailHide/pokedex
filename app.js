4830f7af13042f3656a1eb98cf0093506cef8935
HailHide <hailhide@gmail.com>
2021-03-10 14:17:18 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const container = document.querySelector('#container');
+
+for (let i = 1; i < 899; i++) {
+    const newImg = document.createElement('img');
+    newImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`;
+    container.appendChild(newImg);
+}
